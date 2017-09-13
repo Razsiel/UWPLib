33d7794cae90d17b52674bf86ea0e50233b1d6a1
RAZSIEL <geoffreyarkenbout@gmail.com>
2017-09-13 11:54:20 +0200

    Initial commit: - Added UWP workshop helpers

---
+++ Library/ObservableIncrementalLoadingCollection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Data;
+
+namespace Library
+{
+    public class ObservableIncrementalLoadingCollection<T> : ObservableCollection<T>, ISupportIncrementalLoading
+    {
+        public event OnLoadMoreItems LoadMoreItemsEvent;
+        public delegate List<T> OnLoadMoreItems(uint count);
+
+        private bool _hasMoreItems = true;
+
+        public IAsyncOperation<LoadMoreItemsResult> LoadMoreItemsAsync(uint count)
+        {
+            return Task.Run(async () =>
+            {
+                var list = LoadMoreItemsEvent?.Invoke(count);
+
+                if (list == null || list.Count == 0)
+                {
+                    _hasMoreItems = false;
+                    return new LoadMoreItemsResult { Count = 0 };
+                }
+
+                foreach (var item in list)
+                {
+                    await CoreApplication.MainView.CoreWindow
+                        .Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                        {
+                            Add(item);
+                        });
+                }
+
+                return new LoadMoreItemsResult { Count = (uint)list.Count };
+            }).AsAsyncOperation();
+        }
+
+        public bool HasMoreItems => _hasMoreItems;
+    }
+}
---
+++ Library/RelayCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Input;
+using Windows.Devices.Bluetooth.Background;
+
+namespace Excersise_2.Helpers
+{
+    public sealed class RelayCommand : ICommand
+    {
+        public delegate bool CanExecuteDelegate(object parameter);
+
+        private readonly Action<object> _action;
+        private readonly CanExecuteDelegate _canExecuteDelegate;
+
+        public RelayCommand(Action<object> action, CanExecuteDelegate canExecuteDelegate = null)
+        {
+            _action = action;
+            _canExecuteDelegate = canExecuteDelegate;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecuteDelegate?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action?.Invoke(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
